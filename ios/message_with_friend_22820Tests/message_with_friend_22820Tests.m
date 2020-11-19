4b0de235bd63c3fc35f98e4d7e956c716a285202
Crowdbotics <team@crowdbotics.com>
2020-11-19 22:41:24 +0000

    Initial commit.

---
+++ ios/message_with_friend_22820Tests/message_with_friend_22820Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface message_with_friend_22820 : XCTestCase
+
+@end
+
+@implementation message_with_friend_22820
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
