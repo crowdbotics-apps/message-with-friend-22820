2ed2af022c062ff799d37a42649b6985f8484266
Crowdbotics <team@crowdbotics.com>
2020-11-19 22:42:42 +0000

    Update models

--- backend/message_with_friend_22820/wsgi.py
+++ backend/message_with_friend_22820/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'message_with_friend_22820.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "message_with_friend_22820.settings")
 
 application = get_wsgi_application()
